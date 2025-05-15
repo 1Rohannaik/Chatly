79a9da16c15716b52081649d72720e36122ebedd
1Rohannaik <rohannaik@gmail.com>
2025-05-15 13:42:56 +0530

    Update socket and server configurations

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,35 +1,46 @@
 const express = require("express");
-const {app,server}=require("./lib/socket")
+const { app, server } = require("./lib/socket");
+const path = require("path");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const sequelize = require("./lib/db");
 const port = process.env.PORT || 3000;
-const errorHandler = require("./lib/error")
-const authRoutes = require("./routes/userRoute")
+const errorHandler = require("./lib/error");
+const authRoutes = require("./routes/userRoute");
 const messageRoutes = require("./routes/messageRoute");
 const cors = require("cors");
 
+// Middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",  
-    ],
+    origin: "https://chatly-hfoo.onrender.com", // only hosted frontend allowed
     credentials: true,
     optionsSuccessStatus: 200,
   })
 );
 
 
-app.use(express.json())
+
+app.use(express.json());
 app.use(cookieParser());
 
-
+// Routes
 app.use("/api/v1/users", authRoutes);
 app.use("/api/v1/message", messageRoutes);
 
- 
+// Production static files
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+// Error handler
 app.use(errorHandler);
 
+// DB + Server
 sequelize
   .sync()
   .then(() => {
@@ -41,4 +52,3 @@ sequelize
   .catch((err) => {
     console.error("DB connection failed:", err);
   });
-
