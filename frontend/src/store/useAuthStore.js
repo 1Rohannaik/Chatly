c6d50dbb23d9942c89cedfe7ee5c72d673396ff3
Rohan Naik <139874770+1Rohannaik@users.noreply.github.com>
2025-05-15 18:03:03 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import axiosInstance from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "https://chatly-backend-tqn8.onrender.com";
+const BASE_URL = "https://chatly-backend-8viu.onrender.com";
 
 
 
